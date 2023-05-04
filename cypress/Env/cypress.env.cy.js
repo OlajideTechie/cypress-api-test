428f5203fb4f394c4e49e520ee21018909e8698e
Olajide Ojo <oolajide91@gmail.com>
2023-05-04 19:09:16 +0100

    add authorization as api-key

---
+++ cypress/Env/cypress.env.cy.js
@@ -0,0 +1,4 @@
+
+export let __ENV = {
+    KEY: "EHWcOylYHdatVu8sOaGF3ovmO2Od1g5d",
+}
---
+++ cypress/e2e/customerKyc.cy.js
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+import { __ENV } from "../Env/cypress.env.cy";
+
+let customer_id;
+let firstName;
+let lastName;
+let idNumber;
+let DOB;
+
+describe('Authentication', () => {
+    
+it('get customer kyc details', () => {
+ cy.api ({
+ method: 'GET',
+ url: 'https://preprod.api.mtn.com/v1/customers/237675889923/kyc?endDate=20220901&startDate=20000801&targetSystem=eva' ,
+ headers: {
+    accept: '*/*',
+    'x-api-key': __ENV.KEY,
+   accept: '*/*',
+   connection: 'keep-alive'
+    },
+}).then(response => {
+    customer_id = response.body.customerId;
+    firstName = response.body.data.firstName;
+    lastName = response.body.data.lastName
+    DOB = response.body.data.dateOfBirth
+    idNumber = response.body.data.idNumber
+
+    //assertion tests
+    //Body returns response code 200
+    expect(response.status).equal(200);
+
+    //Body contains ciustomer id
+    expect(customer_id).equal('237675889923')
+
+    //Body contains customer firstname
+    expect(firstName).equal('PHILIPPE MALTAIS')
+
+    //Body contains customer lastname
+    expect(lastName).equal('BENEDOUE')
+
+    // //Body contains customer unique id number
+     expect(DOB).equal('05/12/1973')
+
+     expect(idNumber).equal('20190320107510220')
+   
+
+   })
+
+});
+
+});
+
+
+
+
